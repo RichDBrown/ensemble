32e9c17eaad1704283fe56ced79ea811e7c347ce
RichDBrown <richarddb602@gmail.com>
2025-12-27 23:22:32 -0500

    Add responsive design to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <main className="flex flex-col gap-y-21 pt-16 pb-6 px-4">
-      <section className="flex flex-col items-center w-full">
+    <main className="flex flex-col items-center gap-y-21 pt-16 pb-6 md:pb-16 px-4">
+      <section className="flex flex-col items-center w-full md:max-w-139.75">
         <div className="flex items-center gap-x-4">
           <Image
             src={Logo}
@@ -16,20 +16,22 @@ export default function LandingPage() {
           />
           <h1 className="text-2xl font-medium">Ensemble</h1>
         </div>
-        <h2 className="mt-16 text-3xl font-medium">Never Cram Again</h2>
-        <p className="mt-4 text-center text-on-surface-variant">
+        <h2 className="mt-16 text-3xl md:text-6xl font-medium">
+          Never Cram Again
+        </h2>
+        <p className="mt-4 text-xl text-center text-on-surface-variant">
           A smarter way to study, built on learning science, not last-minute
           stress.
         </p>
         <Link
           href="/dashboard"
-          className="flex items-center justify-center mt-6 w-full h-12 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
+          className="flex items-center justify-center mt-6 w-full h-12 md:h-14 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
         >
           Get started
         </Link>
       </section>
-      <section className="flex flex-col items-center w-full">
-        <h2 className="text-center text-3xl font-medium">
+      <section className="flex flex-col items-center w-full md:max-w-205.75">
+        <h2 className="text-center text-3xl md:text-6xl font-medium">
           The Problems Students Face
         </h2>
         <div className="flex flex-col w-full gap-y-4 p-6 mt-6 border border-outline-variant rounded-xl">
@@ -47,8 +49,10 @@ export default function LandingPage() {
           </p>
         </div>
       </section>
-      <section className="flex flex-col items-center w-full">
-        <h2 className="text-center text-3xl font-medium">The Promise</h2>
+      <section className="flex flex-col items-center w-full md:max-w-205.75">
+        <h2 className="text-center text-3xl md:text-6xl font-medium">
+          The Promise
+        </h2>
         <div className="flex flex-col w-full gap-y-4 p-6 mt-6 border border-outline-variant rounded-xl">
           <p className="text-xl">Never cram again.</p>
           <p className="text-on-surface-variant">
@@ -58,8 +62,10 @@ export default function LandingPage() {
           </p>
         </div>
       </section>
-      <section className="flex flex-col items-center w-full">
-        <h2 className="text-center text-3xl font-medium">How It Works</h2>
+      <section className="flex flex-col items-center w-full md:max-w-205.75">
+        <h2 className="text-center text-3xl md:text-6xl font-medium">
+          How It Works
+        </h2>
         <p className="self-start mt-6 text-xl">1. Enter your course info</p>
         <p className="self-start mt-2 text-on-surface-variant">
           Add your subject, exam date, and topics.
@@ -76,13 +82,15 @@ export default function LandingPage() {
         </p>
         <Link
           href="/dashboard"
-          className="flex items-center justify-center mt-6 w-full h-12 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
+          className="flex items-center justify-center md:hidden mt-6 w-full h-12 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
         >
           Build my study plan
         </Link>
       </section>
-      <section className="flex flex-col items-center w-full">
-        <h2 className="text-center text-3xl font-medium">Why It Works</h2>
+      <section className="flex flex-col items-center w-full md:max-w-205.75">
+        <h2 className="text-center text-3xl md:text-6xl font-medium">
+          Why It Works
+        </h2>
         <p className="self-start mt-6 text-xl">🧠 Spaced repetition</p>
         <p className="self-start mt-2 text-on-surface-variant">
           You review material at carefully chosen intervals so it moves into
@@ -103,16 +111,16 @@ export default function LandingPage() {
           learns.
         </p>
       </section>
-      <section className="flex flex-col items-center w-full">
-        <h2 className="text-center text-3xl font-medium">
+      <section className="flex flex-col items-center w-full md:max-w-205.75">
+        <h2 className="text-center text-3xl md:text-6xl font-medium">
           Start Studying the Way Your Brain is Designed to Learn
         </h2>
-        <p className="mt-4 text-xl text-on-surface-variant">
+        <p className="mt-4 text-xl md:text-xl text-on-surface-variant">
           Never cram again.
         </p>
         <Link
           href="/dashboard"
-          className="flex items-center justify-center mt-6 w-full h-12 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
+          className="flex items-center justify-center mt-6 w-full h-12 md:h-14 text-sm font-medium text-on-primary bg-primary rounded-2xl hover:bg-[#AFCFFB] active:bg-[#AFCFFB] transition-colors"
         >
           Create my study plan
         </Link>
